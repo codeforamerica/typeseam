3bc67edd853d90d4038dccdcaa4ef7a579981868
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-12-31 17:03:33 -0800

    new script for loading fake data

---
+++ typeseam/scripts/load_fake_data.py
@@ -0,0 +1,14 @@
+import sys
+from typeseam.app import create_app
+from tests.mock.factories import generate_fake_data
+
+class MissingEmailError(Exception):
+    pass
+
+def run():
+    app = create_app()
+    with app.app_context():
+        generate_fake_data()
+
+if __name__ == '__main__':
+    run()
