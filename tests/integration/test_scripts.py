e1688b1ff9e58279138d97a3b7b4bc08f613aaa6
Benjamin Golder <benjamin.j.golder@gmail.com>
2016-03-11 13:08:13 -0800

    update add user w responses script with test

---
+++ tests/integration/test_scripts.py
@@ -0,0 +1,27 @@
+import subprocess
+
+from tests.test_base import BaseTestCase
+from typeseam.app import db
+from typeseam.form_filler.models import TypeformResponse
+from typeseam.auth.models import User
+
+
+class TestScripts(BaseTestCase):
+
+    def setUp(self):
+        BaseTestCase.setUp(self)
+
+    def test_add_user_with_responses(self):
+        subprocess.call([
+            'python',
+            'typeseam/scripts/add_user_with_responses.py',
+            'myemail@someplace.org',
+            'p4ssw0rd'
+            ])
+        user = db.session.query(User).filter(User.email=='myemail@someplace.org').first()
+        self.assertTrue(user)
+        responses = db.session.query(TypeformResponse).all()
+        self.assertEqual(len(responses), 20)
+
+
+
--- typeseam/scripts/add_user_with_responses.py
+++ typeseam/scripts/add_user_with_responses.py
@@ -1,13 +1,13 @@
 import sys
 import os
 import typeseam
+from typeseam import constants
 from typeseam.app import create_app
 from typeseam.auth.queries import create_user
 from typeseam.form_filler.queries import create_typeform
 path = os.path.dirname(os.path.dirname(typeseam.__file__))
 sys.path.insert(0, path)
 from tests.mock.factories import generate_fake_responses
-
 
 def run(email=None, password=None):
 
@@ -18,10 +18,11 @@ def run(email=None, password=None):
         title = os.environ.get('DEFAULT_TYPEFORM_TITLE', '')
         live_url = os.environ.get('DEFAULT_TYPEFORM_LIVE_URL', '')
         edit_url = os.environ.get('DEFAULT_TYPEFORM_EDIT_URL', '')
-        form = create_typeform(form_key, title, user, live_url=live_url, edit_url=edit_url)
+        translator_key = os.environ.get('DEFAULT_TYPEFORM_TRANSLATOR', 'TRANSLATOR_A')
+        translator = getattr(constants, translator_key)
+        form = create_typeform(form_key, title=title, user_id=user.id, live_url=live_url,
+            edit_url=edit_url, translator=translator)
         generate_fake_responses(form, 20)
-
-
 
 if __name__ == '__main__':
     run(sys.argv[1], sys.argv[2])
