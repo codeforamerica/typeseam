77ddb8e4aafe8f6e3c968c4fa614449337fb0a02
Ben Golder <bgolder@codeforamerica.org>
2016-04-20 11:37:52 -0700

    added twilio voicemail notification

--- typeseam/public/views.py
+++ typeseam/public/views.py
@@ -1,8 +1,47 @@
 
-from flask import render_template
+import os
+import requests
+from datetime import datetime
+from pytz import timezone
+from flask import render_template, url_for, current_app
 from typeseam.public import blueprint
+from twilio import twiml
+import sendgrid
+from typeseam.app import sg
+
 
 
 @blueprint.route('/privacy/')
 def privacy_policy():
     return render_template('privacy_policy.html')
+
+
+@blueprint.route('/voicemail/', methods=['GET'])
+def voicemail_response():
+    response = twiml.Response()
+    # get the static URL
+    greeting_path = os.path.join(current_app.static_url_path, 'voicemail/CMR_voicemail_greeting.m4a')
+    handle_voicemail_url = url_for(
+            'public.handle_voicemail_recording', _external=True)
+    response.play(greeting_path)
+    response.record(
+        action=handle_voicemail_url,
+        method='POST')
+    return str(response)
+
+
+@blueprint.route('/record/', methods=['POST'])
+def handle_voicemail_recording():
+    recording_url = request.values.get("RecordingUrl", '')
+    from_number = request.values.get("From", '')
+    duration = request.values.get("RecordingDuration", '')
+    time_received = timezone('GMT').localize(
+        datetime.utcnow()).astimezone(timezone('US/Pacific'))
+    text = "Listen to the recording at {}".format(recording_url)
+    # get the recording, attach it to the email
+    message = sendgrid.Mail(
+        subject="New voicemail from {} received {}".format(
+            from_number,
+            time_received.strftime("%-m/%-d/%Y %-I:%M %p %Z")),
+        to=app.config['DEFAULT_NOTIFICATION_EMAIL'],
+        text=text)
